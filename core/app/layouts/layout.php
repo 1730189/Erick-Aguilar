8e6967306dce3255ad0ec0130cacf74478b7b86f
1730189 <55115762+1730189@users.noreply.github.com>
2020-08-11 12:46:53 -0500

    Primer Commit

---
+++ core/app/layouts/layout.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+  <title>Consultorio Medico</title>
+
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/material-dashboard.css" rel="stylesheet"/>
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <script src="assets/js/jquery.min.js" type="text/javascript"></script>
+
+<?php if(isset($_GET["view"]) && $_GET["view"]=="home"):?>
+<link href='assets/fullcalendar/fullcalendar.min.css' rel='stylesheet' />
+<link href='assets/fullcalendar/fullcalendar.print.css' rel='stylesheet' media='print' />
+<script src='assets/fullcalendar/moment.min.js'></script>
+<script src='assets/fullcalendar/fullcalendar.min.js'></script>
+<?php endif; ?>
+
+</head>
+
+<body>
+<?php if(isset($_SESSION["user_id"])):?>
+  <div class="wrapper">
+
+      <div class="sidebar" data-color="blue">
+      <div class="logo">
+        <a href="./" class="simple-text">
+          Consultorio
+        </a>
+      </div>
+
+        <div class="sidebar-wrapper">
+              <ul class="nav">
+                  <li class="">
+                      <a href="./">
+                        <p>Inicio</p>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="./?view=reservations">
+                        <p>Citas</p>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="./?view=pacients">
+                        <p>Pacientes</p>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="./?view=medics">
+                        <p>Medicos</p>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="./?view=categories">
+                        <p>Categorias</p>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="./?view=users">
+                        <p>Usuarios</p>
+                      </a>
+                  </li>
+              </ul>
+        </div>
+      </div>
+
+      <div class="main-panel">
+      <nav class="navbar navbar-transparent navbar-absolute">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="./"><b>Sistema de Citas Medicas</b></a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                  <i class="fa fa-user"></i>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a href="logout.php">Salir</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div class="content">
+      <div class="container-fluid">
+<?php 
+  View::load("login");
+
+?>
+</div>
+      </div>
+    </div>
+  </div>
+<?php else:?>
+  <?php 
+  View::load("login");
+
+?>
+
+<?php endif;?>
+</body>
+
+  <!--   Core JS Files   -->
+  <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+  <script src="assets/js/material.min.js" type="text/javascript"></script>
+
+  <!--  Charts Plugin -->
+  <script src="assets/js/chartist.min.js"></script>
+
+  <!--  Notifications Plugin    -->
+  <script src="assets/js/bootstrap-notify.js"></script>
+
+  <!--  Google Maps Plugin    -->
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+
+  <!-- Material Dashboard javascript methods -->
+  <script src="assets/js/material-dashboard.js"></script>
+
+  <!-- Material Dashboard DEMO methods, don't include it in your project! -->
+  <script src="assets/js/demo.js"></script>
+
+  <script type="text/javascript">
+      $(document).ready(function(){
+
+      // Javascript method's body can be found in assets/js/demos.js
+          demo.initDashboardPageCharts();
+
+      });
+  </script>
+
+</html>
